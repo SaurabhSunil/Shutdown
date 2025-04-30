fe9fe2e566a4399e4916ab4df97169a6401037b3
Saurabh Sunil <ssunil@intelliflo.com>
2025-04-30 17:06:02 +0530

    Add project files.

---
+++ Shutdown/HostedService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Shutdown
+{
+    public sealed class HostedService : IHostedService
+    {
+        public HostedService()
+        {
+        }
+
+        /// <inheritdoc />
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Starting my Application...");
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stopping my Application...");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Shutdown/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shutdown;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<HostedService>();
+        services.Configure<HostOptions>(options =>
+        {
+            // Set the timeout for graceful shutdown to 10 seconds
+            options.ShutdownTimeout = TimeSpan.FromSeconds(30);
+
+            options.ServicesStopConcurrently = true;
+            //options.ServicesStartConcurrently = true;
+            //options.ServicesStopConcurrently = true;
+        });
+    })
+    .Build();
+
+await host.RunAsync();
